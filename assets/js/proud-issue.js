d3cf337c41c4a0e255af262f80af5e0ba907c6df
Jeff Lyon <jeff@albatrossdigital.com>
2016-10-13 14:17:48 -0700

    Cleanup console.log

--- assets/js/proud-issue.js
+++ assets/js/proud-issue.js
@@ -5,12 +5,7 @@
 
 function changeType() {
   $('#agency_url_wrapper, #post_menu_wrapper, #wr_editor_tabs, .wr-editor-tab-content').hide();
-  //if (isNewPost) {
-  //  window.setTimeout(function(){$('#wr_editor_tabs a[href="#wr_editor_tab2"]').trigger('click');}, 1000);
-  //}
   var type = $('input[name=issue_category_type]:checked').val();
-  console.log(type);
-  console.log($('#wp-content-wrap'));
   if (type == 'link' || type == undefined) {
     $('#wp-content-wrap, #post-status-info').hide();
   }
