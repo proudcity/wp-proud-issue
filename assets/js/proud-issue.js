da2d4685ecfb88e39ca068f3809b87fb96fb65a0
Jeff Lyon <jeff@albatrossdigital.com>
2016-10-24 10:51:34 -0700

    Cleaning up logic in commit c27c0fe344f2b9eb95f0e44e61972179735cfd17 that caused Editor to be hidden site-wide https://github.com/proudcity/wp-proudcity/issues/587

--- assets/js/proud-issue.js
+++ assets/js/proud-issue.js
@@ -2,25 +2,26 @@
   Proud.behaviors.proud_issue = {
     attach: function(context, settings) {
 
+      if ($('body.post-type-issue').length) {
+        changeType();
+        $('input[name=issue_category_type]').bind('click', changeType);
 
-function changeType() {
-  $('#agency_url_wrapper, #post_menu_wrapper, #wr_editor_tabs, .wr-editor-tab-content').hide();
-  var type = $('input[name=issue_category_type]:checked').val();
-  if (type == 'link' || type == undefined) {
-    $('#wp-content-wrap, #post-status-info').hide();
-  }
-  else {
-    $('#wp-content-wrap, #post-status-info').show();
-  }
+        $('#issue_meta_box').appendTo('#titlediv').css('margin-top', '1em');
+        $('#wpseo_meta').hide();
+      }
 
-}
+      function changeType() {
+        $('#agency_url_wrapper, #post_menu_wrapper, #wr_editor_tabs, .wr-editor-tab-content').hide();
+        var type = $('input[name=issue_category_type]:checked').val();
+        if (type == 'link' || type == undefined) {
+          $('#wp-content-wrap, #post-status-info').hide();
+        }
+        else {
+          $('#wp-content-wrap, #post-status-info').show();
+        }
 
-changeType();
-$('input[name=issue_category_type]').bind('click', changeType);
-
-$('#issue_meta_box').appendTo('#titlediv').css('margin-top', '1em');
-$('#wpseo_meta').hide();
-
+        
+      }
 
     }
   };
