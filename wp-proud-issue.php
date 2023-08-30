8fd7b123c89442f7e0d0dd3df53fea5928c8a006
Curtis McHale <curtis@curtismchale.ca>
2023-08-30 07:36:17 -0700

    adding new icon for issues

--- wp-proud-issue.php
+++ wp-proud-issue.php
@@ -3,7 +3,7 @@
 Plugin Name: Proud Issue
 Plugin URI: http://proudcity.com/
 Description: Declares an Issue custom post type.
-Version: 1.0
+Version: 2023.08.30.0734
 Author: ProudCity
 Author URI: http://proudcity.com/
 License: Affero GPL v3
@@ -33,45 +33,46 @@ class ProudIssue extends \ProudPlugin {
   }
 
 
-  public function create_issue() {
-      $labels = array(
-          'name'               => _x( 'Issues', 'post name', 'wp-issue' ),
-          'singular_name'      => _x( 'Issue', 'post type singular name', 'wp-issue' ),
-          'menu_name'          => _x( 'Issues', 'admin menu', 'wp-issue' ),
-          'name_admin_bar'     => _x( 'Issue', 'add new on admin bar', 'wp-issue' ),
-          'add_new'            => _x( 'Add New', 'issue', 'wp-issue' ),
-          'add_new_item'       => __( 'Add New Issue', 'wp-issue' ),
-          'new_item'           => __( 'New Issue', 'wp-issue' ),
-          'edit_item'          => __( 'Edit Issue', 'wp-issue' ),
-          'view_item'          => __( 'View Issue', 'wp-issue' ),
-          'all_items'          => __( 'All Issues', 'wp-issue' ),
-          'search_items'       => __( 'Search issue', 'wp-issue' ),
-          'parent_item_colon'  => __( 'Parent issue:', 'wp-issue' ),
-          'not_found'          => __( 'No issues found.', 'wp-issue' ),
-          'not_found_in_trash' => __( 'No issues found in Trash.', 'wp-issue' )
-      );
+	public function create_issue() {
+		$labels = array(
+			'name'               => _x( 'Issues', 'post name', 'wp-issue' ),
+			'singular_name'      => _x( 'Issue', 'post type singular name', 'wp-issue' ),
+			'menu_name'          => _x( 'Issues', 'admin menu', 'wp-issue' ),
+			'name_admin_bar'     => _x( 'Issue', 'add new on admin bar', 'wp-issue' ),
+			'add_new'            => _x( 'Add New', 'issue', 'wp-issue' ),
+			'add_new_item'       => __( 'Add New Issue', 'wp-issue' ),
+			'new_item'           => __( 'New Issue', 'wp-issue' ),
+			'edit_item'          => __( 'Edit Issue', 'wp-issue' ),
+			'view_item'          => __( 'View Issue', 'wp-issue' ),
+			'all_items'          => __( 'All Issues', 'wp-issue' ),
+			'search_items'       => __( 'Search issue', 'wp-issue' ),
+			'parent_item_colon'  => __( 'Parent issue:', 'wp-issue' ),
+			'not_found'          => __( 'No issues found.', 'wp-issue' ),
+			'not_found_in_trash' => __( 'No issues found in Trash.', 'wp-issue' )
+		);
 
-      $args = array(
-          'labels'             => $labels,
-          'description'        => __( 'Description.', 'wp-issue' ),
-          'public'             => true,
-          'publicly_queryable' => true,
-          'show_ui'            => true,
-          'show_in_menu'       => true,
-          'query_var'          => true,
-          'rewrite'            => array( 'slug' => 'issues' ),
-          'capability_type'    => 'post',
-          'has_archive'        => false,
-          'hierarchical'       => false,
-          'menu_position'      => null,
-          'show_in_rest'       => true,
-          'rest_base'          => 'issues',
-          'rest_controller_class' => 'WP_REST_Posts_Controller',
-          'supports'           => array( 'title', 'editor' )
-      );
+		$args = array(
+			'labels'             => $labels,
+			'description'        => __( 'Description.', 'wp-issue' ),
+			'public'             => true,
+			'publicly_queryable' => true,
+			'show_ui'            => true,
+			'show_in_menu'       => true,
+			'menu_icon' 		=> 'dashicons-warning',
+			'query_var'          => true,
+			'rewrite'            => array( 'slug' => 'issues' ),
+			'capability_type'    => 'post',
+			'has_archive'        => false,
+			'hierarchical'       => false,
+			'menu_position'      => null,
+			'show_in_rest'       => true,
+			'rest_base'          => 'issues',
+			'rest_controller_class' => 'WP_REST_Posts_Controller',
+			'supports'           => array( 'title', 'editor' )
+		);
 
-      register_post_type( 'issue', $args );
-  }
+		register_post_type( 'issue', $args );
+	}
 
   public function issue_rest_support() {
     register_rest_field( 'issue',
